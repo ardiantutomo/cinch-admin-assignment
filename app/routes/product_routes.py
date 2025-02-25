33e035204c91f1ecbbaaeb4d336188ec955ccb78
Ardiant Utomo <ardiantutomo@gmail.com>
2025-02-25 16:23:29 +0700

    added auth to product routes

--- app/routes/product_routes.py
+++ app/routes/product_routes.py
@@ -1,5 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Security
 from app.controllers.product_controller import router as product_router
+from app.auth import token_validation
+from fastapi.security import HTTPBearer
 
+api_key_header = HTTPBearer()
 router = APIRouter()
-router.include_router(product_router, prefix="/products", tags=["products"])
+
+router.include_router(
+    product_router, prefix="/products", tags=["products"], dependencies=[Depends(token_validation), Security(api_key_header)])
